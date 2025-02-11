d452962deb2b25f29da28c7c9c54b0ef83e7f3bf
Juan Felipe  J.Z. Zea Sanchez <jzea@db-system.corp>
2025-02-11 12:28:12 -0500

    Create a form and show the data in console

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,13 +3,18 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
+// Import Angular Forms classes
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
+import { first, last } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
@@ -21,9 +26,26 @@ export class DetailsComponent {
   housingLocationId = -1;
   housingLocation: HousingLocation | undefined;
 
+  // Create a FormGroup instance to manage the form data.
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor()
   {
     this.housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
+
+  submitApplication()
+  {
+    this.housingService.submitApplication(
+      // With this syntax, we can access to form data.
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
