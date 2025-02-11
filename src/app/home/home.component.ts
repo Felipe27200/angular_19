9741ee74958d1d9eda288e43831406cff2cf2001
Juan Felipe  J.Z. Zea Sanchez <jzea@db-system.corp>
2025-02-11 12:49:25 -0500

    Make a filter to the houses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,9 +25,25 @@ export class HomeComponent {
    * wihtout the need of do it in the constructor.
    */
   housingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor()
   {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) 
+  {
+    if (!text) 
+    {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList
+      .filter((housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+      );
   }
 }
